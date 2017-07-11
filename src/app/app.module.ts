697a473ccbf46930b1a89d270a080a3c61bac14d
Michael Bowman <bowmanmc@gmail.com>
2017-07-11 16:22:42 +0000

    Application layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        SidebarComponent,
+        ContentComponent
     ],
     imports: [
         BrowserModule
