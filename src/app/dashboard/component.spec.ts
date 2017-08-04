3044ef6b4550db8ea2612aceaae0c8590df58b59
Michael Bowman <bowmanmc@gmail.com>
2017-08-04 10:39:09 -0400

    fixed dashboard component spec

--- src/app/dashboard/component.spec.ts
+++ src/app/dashboard/component.spec.ts
@@ -1,16 +1,25 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { HttpModule } from '@angular/http';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { DashboardApiService } from './api.service';
 import { DashboardComponent } from './component';
 
+
 describe('DashboardComponent', () => {
+
     let component: DashboardComponent;
     let fixture: ComponentFixture<DashboardComponent>;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [DashboardComponent]
+            declarations: [DashboardComponent],
+            imports: [HttpModule],
+            providers: [DashboardApiService],
+            // Tells the compiler not to error on unknown elements and attributes
+            schemas: [NO_ERRORS_SCHEMA]
         })
-            .compileComponents();
+        .compileComponents();
     }));
 
     beforeEach(() => {
