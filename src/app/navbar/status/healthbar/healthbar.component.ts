c02faf27cf0d7b48e7359448f47de129100bc2b4
ericsvendsen <esvendsen@gmail.com>
2017-08-08 15:28:20 -0400

    fixing lint errors

--- src/app/navbar/status/healthbar/healthbar.component.ts
+++ src/app/navbar/status/healthbar/healthbar.component.ts
@@ -16,15 +16,13 @@ export class HealthbarComponent implements OnInit {
     }
 
     getBarClass() {
-        let className = "healthbar__status ";
+        let className = 'healthbar__status ';
         if (this.percentage >= 90) {
-            className += "healthbar__status-error";
-        }
-        else if (this.percentage >= 70) {
-            className += "healthbar__status-warn";
-        }
-        else {
-            className += "healthbar__status-good";
+            className += 'healthbar__status-error';
+        } else if (this.percentage >= 70) {
+            className += 'healthbar__status-warn';
+        } else {
+            className += 'healthbar__status-good';
         }
         return className;
     }
