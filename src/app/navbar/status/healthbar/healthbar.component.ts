9a2563d0dc707771cba38da6e81106f9ae9be1f5
Michael Bowman <bowmanmc@gmail.com>
2017-08-09 13:13:32 +0000

    Use a gradient on the healthbar instead of bins to better show danger

--- src/app/navbar/status/healthbar/healthbar.component.ts
+++ src/app/navbar/status/healthbar/healthbar.component.ts
@@ -14,19 +14,10 @@ export class HealthbarComponent implements OnInit {
     ngOnInit() {
     }
 
-    getBarClass() {
-        let className = 'healthbar__status ';
-        if (this.percentage >= 90) {
-            className += 'healthbar__status-error';
-        } else if (this.percentage >= 70) {
-            className += 'healthbar__status-warn';
-        } else {
-            className += 'healthbar__status-good';
-        }
-        return className;
-    }
-
     getBarWidth() {
-        return `${this.percentage}%`;
+        if (this.percentage) {
+            return `${100 - this.percentage}%`;
+        }
+        return '0%';
     }
 }
