c958bebb414094ed6b541bf3fd346d792298fc94
Michael Bowman <bowmanmc@gmail.com>
2017-08-09 14:35:04 -0400

    navigation tweaks

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-    private selectedId = null;
+    private selectedId = 'configuration';
 
     constructor() {
     }
