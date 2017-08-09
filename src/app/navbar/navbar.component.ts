30c6cdfa8cd88ffdf75aa3d05797f88ff09856d7
Michael Bowman <bowmanmc@gmail.com>
2017-08-09 15:14:18 -0400

    fixing some nav line issues

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-    private selectedId = 'configuration';
+    private selectedId = null;
 
     constructor() {
     }
