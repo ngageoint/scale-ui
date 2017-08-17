e70f3fb0cee99e717e0dcdc1b899e06e6483e654
Michael Bowman <bowmanmc@gmail.com>
2017-08-17 15:09:07 -0400

    status colors for job types

--- src/app/dashboard/jobtypeitem/jobtypeitem.component.ts
+++ src/app/dashboard/jobtypeitem/jobtypeitem.component.ts
@@ -32,4 +32,36 @@ export class JobtypeitemComponent implements OnInit {
         }
         return 0;
     }
+
+    getStatusClass() {
+        const counts = this.item.job_counts;
+
+        let failed = 0;
+        let completed = 0;
+        for (let i = 0; i < counts.length; i++) {
+            if (counts[i].status === 'COMPLETED') {
+                completed += counts[i].count;
+            }
+            if (counts[i].status === 'FAILED') {
+                failed += counts[i].count;
+            }
+        }
+
+        let total = failed + completed;
+        let successRate = 100;
+
+        if (failed > 0) {
+            successRate = 100 - ((failed / total) * 100);
+        }
+
+        let cssClass = 'jti__status-good';
+        if (successRate <= 80) {
+            cssClass = 'jti__status-warn';
+        }
+        if (successRate <= 60) {
+            cssClass = 'jti__status-error';
+        }
+
+        return cssClass;
+    }
 }
