11dd6ad9d3917aa2b5f5c883f2aa79da92842f70
Michael Bowman <bowmanmc@gmail.com>
2017-08-22 13:49:54 -0400

    fixing unit tests

--- src/app/dashboard/jobtypeitem/jobtypeitem.component.spec.ts
+++ src/app/dashboard/jobtypeitem/jobtypeitem.component.spec.ts
@@ -1,25 +1,28 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { DashboardFavoritesService } from '../favorites.service';
 import { JobtypeitemComponent } from './jobtypeitem.component';
 
+
 describe('JobtypeitemComponent', () => {
-  let component: JobtypeitemComponent;
-  let fixture: ComponentFixture<JobtypeitemComponent>;
+    let component: JobtypeitemComponent;
+    let fixture: ComponentFixture<JobtypeitemComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ JobtypeitemComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [JobtypeitemComponent],
+            providers: [DashboardFavoritesService]
+        })
+        .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(JobtypeitemComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(JobtypeitemComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(component).toBeTruthy();
+    });
 });
