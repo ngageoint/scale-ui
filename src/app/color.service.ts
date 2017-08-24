3e00d4524d54e68caf96a17b7414ae257548e0b7
Michael Bowman <bowmanmc@gmail.com>
2017-08-24 10:37:49 -0400

    tweaking the colors just a bit more

--- src/app/color.service.ts
+++ src/app/color.service.ts
@@ -3,11 +3,11 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ColorService {
 
-    public COMPLETED = '#618058';
-    public ERROR = '#9d3f2e';
-    public ERROR_DATA = '#9d3f2e';
-    public ERROR_ALGORITHM = '#9d642e';
-    public ERROR_SYSTEM = '#9d892e';
+    public COMPLETED = '#576f50';
+    public ERROR = '#88382a';
+    public ERROR_DATA = '#88382a';
+    public ERROR_ALGORITHM = '#88582A';
+    public ERROR_SYSTEM = '#88772A';
 
     constructor() { }
 }
