adace805219bc6ce7fe38ecceefe0dcd3b928156
Michael Bowman <bowmanmc@gmail.com>
2017-08-29 10:03:45 -0400

    initial state for subnav

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-    private selectedId = 'processing';
+    private selectedId = null;
 
     constructor() {
     }
