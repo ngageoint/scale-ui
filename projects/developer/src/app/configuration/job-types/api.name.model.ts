5aafa3e2d1dac4b26d114a9bee2b3e9f8829224b
ericsvendsen <esvendsen@gmail.com>
2019-01-22 10:39:59 -0500

    added id back to job types list; changed icon for workspaces

--- projects/developer/src/app/configuration/job-types/api.name.model.ts
+++ projects/developer/src/app/configuration/job-types/api.name.model.ts
@@ -2,6 +2,7 @@ export class JobTypeName {
     private static build(data) {
         if (data) {
             return new JobTypeName(
+                data.id,
                 data.name,
                 data.title,
                 data.description,
@@ -21,18 +22,12 @@ export class JobTypeName {
         return null;
     }
     constructor(
-        public name?: string,
-        public title?: string,
-        public description?: string,
-        public icon_code?: string,
-        public num_versions?: number,
-        public latest_version?: string
-    ) {
-        this.name = this.name || null;
-        this.title = this.title || null;
-        this.description = this.description || null;
-        this.icon_code = this.icon_code || null;
-        this.num_versions = this.num_versions || null;
-        this.latest_version = this.latest_version || null;
-    }
+        public id: number,
+        public name: string,
+        public title: string,
+        public description: string,
+        public icon_code: string,
+        public num_versions: number,
+        public latest_version: string
+    ) {}
 }
