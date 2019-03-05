be1d9690ee1de88b2745d6ffad30a273dff02b52
ericsvendsen <esvendsen@gmail.com>
2019-03-05 10:18:16 -0500

    adding missing dependency

--- projects/developer/src/app/configuration/recipe-types/api.service.ts
+++ projects/developer/src/app/configuration/recipe-types/api.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import * as _ from 'lodash';
 
 import { Observable } from 'rxjs';
 import { catchError, map } from 'rxjs/internal/operators';
