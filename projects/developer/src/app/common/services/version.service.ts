61ac7c98b01dd69a07d047297d648ed71d249f38
Rachel Hill <rachel.hill@appliedis.com>
2019-05-23 17:37:31 -0400

    adding API version to footer

---
+++ projects/developer/src/app/common/services/version.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/internal/operators';
+
+import { DataService } from '../../common/services/data.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VersionService {
+  apiPrefix: string;
+
+  constructor(
+    private http: HttpClient
+  ) {
+    this.apiPrefix = DataService.getApiPrefix('version');
+  }
+
+  getVersion() {
+    return this.http.get(`${this.apiPrefix}/version/`)
+    .pipe(
+      catchError(DataService.handleError)
+    );
+  }
+}
