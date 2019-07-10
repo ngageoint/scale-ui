903877e46feb7ccb84e8d6c17b4bb54c0e969af7
Rachel <rachel.hill@appliedis.com>
2019-07-10 07:35:19 -0400

    remove console log

--- projects/developer/src/app/system/workspaces/component.ts
+++ projects/developer/src/app/system/workspaces/component.ts
@@ -228,7 +228,6 @@ export class WorkspacesComponent implements OnInit, OnDestroy {
     }
 
     onValidateClick() {
-        console.log(this.selectedWorkspaceDetail);
         this.workspacesApiService.validateWorkspace(this.selectedWorkspaceDetail).subscribe(data => {
             this.validated = data.is_valid;
             if (data.is_valid) {
