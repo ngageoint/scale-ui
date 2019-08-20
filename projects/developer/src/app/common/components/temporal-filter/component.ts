0d78f1a4325c2f849941d198e16b29188dcdde82
Mike Holt <mike.n.holt@gmail.com>
2019-08-20 13:40:34 -0400

    converted all buttons to optional

--- projects/developer/src/app/common/components/temporal-filter/component.ts
+++ projects/developer/src/app/common/components/temporal-filter/component.ts
@@ -12,16 +12,15 @@ import { environment } from '../../../../environments/environment';
 export class TemporalFilterComponent implements OnInit {
     @Input() started: string;
     @Input() ended: string;
+    @Input() show6hButton = true;
+    @Input() show12hButton = true;
+    @Input() show24hButton = true;
+    @Input() show3dButton = true;
+    @Input() show7dButton = true;
     @Output() dateFilterApply: EventEmitter<any> = new EventEmitter();
     @Output() dateRangeSelected: EventEmitter<any> = new EventEmitter();
     dateFormat = environment.dateFormat;
-    dateRangeOptions = [
-        { label: 'Last 6 Hours', value: { unit: 'h', range: 6 } },
-        { label: 'Last 12 Hours', value: { unit: 'h', range: 12 } },
-        { label: 'Last 24 Hours', value: { unit: 'h', range: 24 } },
-        { label: 'Last 3 Days', value: { unit: 'd', range: 3 } },
-        { label: 'Last 7 Days', value: { unit: 'd', range: 7 } }
-    ];
+    dateRangeOptions = [];
     selectedDateRange: any;
     applyBtnClass = 'ui-button-secondary';
     isMobile: boolean;
@@ -64,5 +63,10 @@ export class TemporalFilterComponent implements OnInit {
         this.breakpointObserver.observe(['(min-width: 1275px)']).subscribe((state: BreakpointState) => {
             this.isMobile = !state.matches;
         });
+        if (this.show6hButton) this.dateRangeOptions.push({ label: 'Last 6 Hours', value: { unit: 'h', range: 6 } });
+        if (this.show12hButton) this.dateRangeOptions.push({ label: 'Last 12 Hours', value: { unit: 'h', range: 12 } });
+        if (this.show24hButton) this.dateRangeOptions.push({ label: 'Last 24 Hours', value: { unit: 'h', range: 24 } });
+        if (this.show3dButton) this.dateRangeOptions.push({ label: 'Last 3 Days', value: { unit: 'h', range: 3 } });
+        if (this.show7dButton) this.dateRangeOptions.push({ label: 'Last 7 Days', value: { unit: 'h', range: 7 } });
     }
 }
