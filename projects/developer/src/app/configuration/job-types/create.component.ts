73d9882ec2edf9d619b3335b4d4d9c0c23204117
Rachel <rachel.hill@appliedis.com>
2019-10-14 08:50:04 -0400

    change how url is handled

--- projects/developer/src/app/configuration/job-types/create.component.ts
+++ projects/developer/src/app/configuration/job-types/create.component.ts
@@ -239,8 +239,10 @@ export class JobTypesCreateComponent implements OnInit, OnDestroy, ComponentCanD
         // use the information from seed-images to select the proper version and package
         const job: any = _.find(seedImage.job.JobVersions, { JobVersion: seedImage.selectedJobVersion });
         const image: any = job ? _.find(job.Images, { PackageVersion: seedImage.selectedPackageVersion }) : null;
-        if (job.URL) {
-            this.jobType.docker_image = job.URL;
+        console.log(seedImage);
+        console.log(job);
+        if (seedImage.job.URL) {
+            this.jobType.docker_image = seedImage.job.URL;
         } else {
             this.jobType.manifest = seedImage.manifest;
             if (job && image) {
