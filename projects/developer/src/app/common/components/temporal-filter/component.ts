0aed6e539d3444fdd0eade24f1879c151a41ddd3
Mark Heppner <mheppner@users.noreply.github.com>
2019-11-01 16:37:35 -0400

    Initial redesign using primeng calendar and new logic

--- projects/developer/src/app/common/components/temporal-filter/component.ts
+++ projects/developer/src/app/common/components/temporal-filter/component.ts
@@ -13,60 +13,140 @@ import { environment } from '../../../../environments/environment';
 export class TemporalFilterComponent implements OnInit {
     @Input() started: string;
     @Input() ended: string;
+    @Input() liveRange: number;
     @Output() dateFilterApply: EventEmitter<any> = new EventEmitter();
     @Output() dateRangeSelected: EventEmitter<any> = new EventEmitter();
     dateFormat = environment.dateFormat;
     dateRangeOptions = [
-        { label: 'Last 6 Hours', value: { unit: 'h', range: 6 } },
-        { label: 'Last 12 Hours', value: { unit: 'h', range: 12 } },
-        { label: 'Last 24 Hours', value: { unit: 'h', range: 24 } },
-        { label: 'Last 3 Days', value: { unit: 'd', range: 3 } },
-        { label: 'Last 7 Days', value: { unit: 'd', range: 7 } }
+        { label: '---', value: null },
+        { label: 'Last 1 hour', value: 1 },
+        { label: 'Last 6 hours', value: 6 },
+        { label: 'Last 12 hours', value: 12 },
+        { label: 'Last day', value: 24 },
+        { label: 'Last 3 days', value: 24 * 3 },
+        { label: 'Last week', value: 24 * 7 }
     ];
-    selectedDateRange: any;
-    applyBtnClass = 'ui-button-secondary';
-    applyStatusClass = 'live-range-inactive';
-    applyTxtClass = '';
-    liveRangeTooltip = 'No Live Range Selected';
+
+    startDate: Date;
+    endDate: Date;
+
+
+    get utcStartDate(): Date {
+        return this.localDateToUTC(this.startDate);
+    }
+
+    get utcEndDate(): Date {
+        return this.localDateToUTC(this.endDate);
+    }
+
+    get isLiveMode(): boolean {
+        return !!this.liveRange;
+    }
 
     constructor(
         public breakpointObserver: BreakpointObserver,
         private messageService: MessageService
-    ) {}
+    ) {
+        /*
 
-    onStartSelect(e) {
-        this.started = moment.utc(e, this.dateFormat).startOf('d').format(this.dateFormat);
-        this.applyBtnClass = 'ui-button-primary';
+        TODO:
+        - styles for input groups
+        - verify emit back for live range
+        - save settings to localstorage for switching across pages
+        - remove custom utc calendar?
+        - hook up @input for liveRange
+
+        DESIGN:
+        either in three states when this component initializes:
+        - in normal range mode:
+            - live range is null
+            - start and end are set
+        - in live mode:
+            - live range is set (ignores start and end)
+        - nothing is set:
+            - live range should be kept as null
+            - start/end should use one day range from now
+         */
     }
 
-    onEndSelect(e) {
-        this.ended = moment.utc(e, this.dateFormat).endOf('d').format(this.dateFormat);
-        this.applyBtnClass = 'ui-button-primary';
-    }
 
-    onDateFilterApply() {
-        this.applyBtnClass = 'ui-button-secondary';
-        this.applyTxtClass = 'txt-field-active';
-        this.applyStatusClass = 'live-range-inactive';
-        this.liveRangeTooltip = 'No Live Range Selected';
-        this.dateFilterApply.emit({ started: this.started, ended: this.ended });
+    /**
+     * Callback for when a normal start/end value is applied
+     */
+    onDateFilterApply(): void {
+        // normal date filter was applied, turn off live mode
+        this.liveRange = null;
+
+        // send out the converted date strings
+        this.dateFilterApply.emit({
+            started: this.utcStartDate.toISOString(),
+            ended: this.utcEndDate.toISOString()
+        });
+
         if (this.started > this.ended) {
-            this.applyTxtClass = 'txt-field-error';
+            // TODO validate
             this.messageService.add({severity: 'error', summary: 'Error querying range', detail: 'Provided FROM date is before TO date'});
         }
     }
 
-    setDateFilterRange(unit: any, range: any) {
-        this.applyTxtClass = 'txt-field-inactive';
-        this.applyStatusClass = 'live-range-active';
-        this.liveRangeTooltip = 'Refreshing every 10 seconds';
-        this.started = moment.utc().subtract(range, unit).toISOString();
-        this.ended = moment.utc().toISOString();
-        this.dateRangeSelected.emit({ unit: unit, range: range });
+    /**
+     * Callback for when the live range model changes to either emit the value or reset the filter range.
+     */
+    onLiveRangeChange(): void {
+        if (this.liveRange) {
+            // ensure start/end filters are set to empty
+            this.startDate = null;
+            this.endDate = null;
+            // emit out the date range was selected
+            this.dateRangeSelected.emit({ unit: 'h', range: this.liveRange });
+        } else {
+            // live range not provided, reset the normal start/end filters
+            // TODO logic to pull these better?
+            this.startDate = this.utcDateToLocal(this.started);
+            this.endDate = this.utcDateToLocal(this.ended);
+        }
     }
 
     ngOnInit() {
-        this.selectedDateRange = { unit: 'h', range: 24 } ;
-        this.setDateFilterRange('h', 24);
+        // if not in live mode (no range value set), provide a start/end range for normal filters
+        if (!this.isLiveMode) {
+            // convert from the string value (utc) to local time (remove the timezone)
+            if (this.started && this.ended) {
+                this.startDate = this.utcDateToLocal(this.started);
+                this.endDate = this.utcDateToLocal(this.ended);
+            } else {
+                // ensure at least a day range is provided
+                const now = moment();
+                this.endDate = now.toDate();
+                this.startDate = now.clone().subtract(1, 'day').toDate();
+            }
+        }
+    }
+
+    /**
+     * Converts and strips a date string to local date.
+     * @param  date string or date with timezone component
+     * @return      date object without timezone
+     */
+    private utcDateToLocal(date: string): Date {
+        const v = moment(date).utc();
+        return new Date(
+            v.year(), v.month(), v.date(),
+            v.hours(), v.minutes(), v.seconds(), v.milliseconds()
+        );
+    }
+
+    /**
+     * Converts and strips the given date to a date for UTC time.
+     * @param  date string or date with timezone component
+     * @return      date converted to UTC
+     */
+    private localDateToUTC(date: Date): Date {
+        const v = moment(date);
+        const utc = moment.utc([
+            v.year(), v.month(), v.date(),
+            v.hours(), v.minutes(), v.seconds(), v.milliseconds()
+        ]);
+        return utc.toDate();
     }
 }
