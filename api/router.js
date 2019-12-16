3a72b757d982acda2446cfef2ef734247278e9c2
Steve Busch <steve.busch@appliedis.com>
2019-12-16 12:15:40 -0500

    Adds mocks for datasets.

--- api/router.js
+++ api/router.js
@@ -34,6 +34,7 @@ var batchValidate = require('./handlers/batchValidate');
 var datasets = require('./handlers/datasets');
 var datasetCreate = require('./handlers/datasetCreate');
 var datasetValidate = require('./handlers/datasetValidate');
+var datasetAddMembers = require('./handlers/datasetAddMembers');
 
 var strikes = require('./handlers/strikes');
 var strikeDetails = require('./handlers/strikeDetails');
@@ -315,6 +316,12 @@ module.exports = {
             method: 'POST',
             path: '/mocks/' + apiVersion + '/datasets/validation',
             handler: datasetValidate
+        });
+
+        server.route({
+            method: 'POST',
+            path: '/mocks/' + apiVersion + '/datasets/1',
+            handler: datasetAddMembers
         });
 
         server.route({
