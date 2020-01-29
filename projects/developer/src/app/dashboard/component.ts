b5cdb24ed2a47e00f55053658e59e434798852ca
Rachel <rachel.hill@appliedis.com>
2020-01-29 14:11:06 -0500

    piechart labels now show icons

--- projects/developer/src/app/dashboard/component.ts
+++ projects/developer/src/app/dashboard/component.ts
@@ -5,7 +5,6 @@ import * as _ from 'lodash';
 import { JobsApiService } from '../processing/jobs/api.service';
 import { JobTypesApiService } from '../configuration/job-types/api.service';
 import { DashboardJobsService } from './jobs.service';
-import { QueueApiService } from '../common/services/queue/api.service';
 import { ColorService } from '../common/services/color.service';
 
 
@@ -23,45 +22,12 @@ export class DashboardComponent implements OnInit, OnDestroy {
     columnsAll: any[];
     subscription: any;
     allJobTypes: any;
-    allJobTypesTooltip: string;
     favoriteJobTypes: any[];
-    favoriteJobTypesTooltip: string;
-    pieChartOptions: any;
-    totalAll: number;
-    failedAll: number;
-    errorDataAll: any;
-    totalDataAll: any;
-    totalFavs: number;
-    failedFavs: number;
     dataFavs: any;
-    running: any;
-    pending: any;
-    queued: any;
-    running_count: any;
-    pending_count: any;
-    queued_count: any;
-    dataFeedChartTitle: string;
-    historyChartTitle: string;
-    activityChartTitle: string;
     options: any;
-    totalChartData: any;
-    queueLoadData: any;
-    systemCount = {
-        running: 0,
-        pending: 0,
-        queued: 0
-    };
-    userCount = {
-        running: 0,
-        pending: 0,
-        queued: 0
-    };
-    graph: any;
-    graphFav: any;
 
     dateRangeOptions = [
         { label: 'Last day', value: 24 },
-        { label: 'Last 3 days', value: 24 * 3 },
         { label: 'Last week', value: 24 * 7 }
     ];
 
@@ -69,12 +35,11 @@ export class DashboardComponent implements OnInit, OnDestroy {
         private messageService: MessageService,
         private jobsApiService: JobsApiService,
         private jobTypesApiService: JobTypesApiService,
-        private jobsService: DashboardJobsService,
-        private queueApiService: QueueApiService
+        private jobsService: DashboardJobsService
     ) {
         this.options = {
+            responsive: false,
             cutoutPercentage: 0,
-            borderWidth: 15,
             tooltips: {
                 callbacks: {
                     label: function (item, data) {
@@ -86,10 +51,36 @@ export class DashboardComponent implements OnInit, OnDestroy {
             },
             legend: {
                 display: false,
-                onClick: {
-
-                 }
-            }
+            },
+            rotation: 0.5 * Math.PI, // start from bottom
+            elements: {
+                inner: {
+                    borderWidth: 4
+                }
+            },
+            plugins: {
+                datalabels: {
+                    font: {
+                        family: 'FontAwesome',
+                        weight: 'bold'
+                    },
+                    formatter: function(value, ctx) {
+                        if ( ctx.dataset.label === 'Outer Ring') {
+                            return String.fromCharCode(parseInt(ctx.dataset.icon[ctx.dataIndex], 16));
+                        } else {
+                            if ( value > 0) {
+                                return value;
+                            } else {
+                                return null;
+                            }
+                        }
+                    },
+                    align: 'center',
+                    anchor: 'center',
+                    textAlign: 'center',
+                    color: 'black',
+                }
+            },
         };
         this.columnsFavs = [
             { field: 'title', header: 'Title', filterMatchMode: 'contains' }
@@ -106,7 +97,6 @@ export class DashboardComponent implements OnInit, OnDestroy {
         }
     }
     ngOnInit() {
-        this.getQueueData();
         this.refreshAllJobTypes();
         this.jobsService.favoritesUpdated.subscribe(() => {
             this.refreshAllJobTypes();
@@ -135,7 +125,7 @@ export class DashboardComponent implements OnInit, OnDestroy {
             let chartParams = {};
                 chartParams = {
                     is_active: true,
-                    status: ['RUNNING', 'PENDING', 'QUEUED']
+                    status: ['RUNNING']
                 };
                 this.subscription = this.jobsApiService.getJobs(chartParams).subscribe(chartData => {
                     if (this.favoriteJobTypes) {
@@ -158,105 +148,69 @@ export class DashboardComponent implements OnInit, OnDestroy {
     }
 
     createSunburstChart(data, type) {
-        const pendingLabels = [];
-        const runningLabels = [];
-        const queuedLabels = [];
-        const pendingValues = [];
-        const runningValues = [];
-        const queuedValues = [];
-        const colors = [];
+        const labels = [];
+        const values = [];
+        const iconList = [];
         let tempSysCount = 0;
         let tempUserCount = 0;
 
-        const pendingJobs = _.filter(data, function (r) {
-            return r.status === 'PENDING';
-        });
-        const runningJobs = _.filter(data, function (r) {
-            return r.status === 'RUNNING';
-        });
-        const queuedJobs = _.filter(data, function (r) {
-            return r.status === 'QUEUED';
-        });
-
-
-        _.forEach(pendingJobs, job => {
-            const index = _.findIndex(pendingLabels, function(o) { return o === job.job_type.title; });
-            if ( index >= 0) {
-                pendingValues[index] = (pendingValues[index]) + 1;
-            } else {
-                pendingLabels.push(job.job_type.title);
-                pendingValues.push(1);
-                colors.push(ColorService.PENDING);
-            }
-        });
-        _.forEach(runningJobs, job => {
-            // const index = _.findIndex(labels, function(o) { return o === job.job_type.title; });
-            const index = _.findIndex(runningLabels, function(o) { return o === job.job_type.title; });
-            if (index >= 0) {
-                runningValues[index] = (runningValues[index]) + 1;
-            } else {
-                runningLabels.push(job.job_type.title);
-                runningValues.push(1);
-                colors.push(ColorService.RUNNING);
-            }
-        });
-        _.forEach(queuedJobs, job => {
-            const queuedIndex = _.findIndex(queuedLabels, function(o) { return o === job.job_type.title; });
-            if ( queuedIndex >= 0) {
-                queuedValues[queuedIndex] = (queuedValues[queuedIndex]) + 1;
-            } else {
-                queuedLabels.push(job.job_type.title);
-                queuedValues.push(1);
-                colors.push(ColorService.QUEUED);
-            }
-        });
-        const labels = _.concat(pendingLabels, runningLabels, queuedLabels);
-        const values = _.concat(pendingValues, runningValues, queuedValues);
-        // console.log(labels)
         _.forEach(data, job => {
-            if ( job.job_type.is_system ) {
-            tempSysCount++;
+            const index = _.findIndex(labels, function(o) { return o === job.job_type.title; });
+            if (index >= 0) {
+                values[index] = (values[index]) + 1;
             } else {
-            tempUserCount++;
+                labels.push(job.job_type.title);
+                values.push(1);
+                iconList.push(job.job_type.icon_code);
             }
         });
+
+
+        _.forEach(data, job => {
+            if (job.job_type.is_system) {
+                tempSysCount++;
+            } else {
+                tempUserCount++;
+            }
+        });
+
         if (type === 'fav') {
-            this.systemCount.running = tempSysCount;
-            this.userCount.running = tempUserCount;
-        this.running = {
-            labels: ['system', 'user', 'running', 'pending', 'queued'],
+        this.dataFavs = {
+            labels: ['system', 'user', 'running'],
             borderWidth: 30,
             datasets: [
                 {
                     data: values,
-                    label: 'Data 2',
+                    label: 'Outer Ring',
                     labels: labels,
-                    backgroundColor: colors
+                    icon: iconList,
+                    backgroundColor: ColorService.RUNNING
                 },
                 {
-                    data: [this.systemCount.running, this.userCount.running],
-                    label: 'Data 1',
+                    data: [tempSysCount, tempUserCount],
+                    label: 'Inner Ring',
                     labels: ['System', 'User'],
+                    icon: null,
                     backgroundColor: [ColorService.SCALE_BLUE1, ColorService.SCALE_BLUE2]
                 },
             ],
         };
         } else if (type === 'all') {
-            this.systemCount.pending = tempSysCount;
-            this.userCount.pending = tempUserCount;
-            this.pending = {
-                labels: ['Pending'],
+            this.data = {
+                labels: ['system', 'user', 'running'],
                 datasets: [
                     {
                         data: values,
-                        label: 'Data 2',
+                        label: 'Outer Ring',
                         labels: labels,
-                        backgroundColor: colors
+                        icon: iconList,
+                        backgroundColor: ColorService.RUNNING
                     },
                     {
-                        data: [this.systemCount.running, this.userCount.running],
-                        label: 'Data 1',
+                        data: [tempSysCount, tempUserCount],
+                        label: 'Inner Ring',
                         labels: ['System', 'User'],
+                        icon: null,
                         backgroundColor: [ColorService.SCALE_BLUE1, ColorService.SCALE_BLUE2]
                     }
                 ],
@@ -264,25 +218,6 @@ export class DashboardComponent implements OnInit, OnDestroy {
         }
     }
 
-    getQueueData() {
-        const params = {
-            started: this.started,
-            ended: this.ended,
-        };
-        this.subscription = this.queueApiService.getLoad(params, true).subscribe(data => {
-            this.running_count = 0;
-            this.queued_count = 0;
-            this.pending_count = 0;
-            const lastValue = data.results.length - 1;
-            const lastHour = data.results[lastValue];
-
-            this.running_count = lastHour.running_count;
-            this.queued_count = lastHour.queued_count;
-            this.pending_count = lastHour.pending_count;
-        }, err => {
-            this.messageService.add({severity: 'error', summary: 'Error retrieving queue load', detail: err.statusText});
-        });
-    }
     onTemporalFilterUpdate(data: {start: string, end: string}): void {
         this.started = data.start;
         this.ended = data.end;
