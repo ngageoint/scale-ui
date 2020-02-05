ba12f8bce8e62c953a92f27cc8d4aea2c2d5b416
Mark Heppner <mheppner@users.noreply.github.com>
2020-02-05 15:45:37 -0500

    fix(jobs): only send single array when cancelling all jobs
    fixes #432

--- projects/developer/src/app/processing/jobs/component.ts
+++ projects/developer/src/app/processing/jobs/component.ts
@@ -347,7 +347,7 @@ export class JobsComponent implements OnInit, OnDestroy {
                 status: this.datatableOptions.status === 'RUNNING' || this.datatableOptions.status === 'QUEUED' ?
                     this.datatableOptions.status :
                     null,
-                job_type_names: this.datatableOptions.job_type_name ? [this.datatableOptions.job_type_name] : null
+                job_type_names: this.datatableOptions.job_type_name ? this.datatableOptions.job_type_name : null
             };
             // remove null properties
             jobsParams = _.pickBy(jobsParams);
